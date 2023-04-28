06ed3a65664630151549ee452c75de27d7ee023b
SiveyDev <sivey@protonmail.com>
2023-04-27 22:27:05 -0400

    Create basic structure in html and js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,6 @@
 console.log("Formatter Online");
+var inputText = document.getElementById('txt');
+var format = document.getElementById('format');
+format.addEventListener("click", function(){
+    console.log(inputText.value);
+})
