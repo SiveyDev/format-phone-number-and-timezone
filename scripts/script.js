d7120e6af472e761c96012ae2ac2d7f2a456a496
SiveyDev <sivey@protonmail.com>
2023-05-13 20:25:14 -0400

    Complete format of time.

--- scripts/script.js
+++ scripts/script.js
@@ -6,8 +6,9 @@ let textValue = '';
 format.addEventListener("click", function(){
     textValue = inputText.value;
     let timeHour = textValue[1] + textValue[2];
+    let timeMinute = textValue[4] + textValue[5];
     //p.textContent = textValue;
     //console.log(inputText.value);
-    p.textContent = timeHour - 2;
-    console.log(timeNum);
+    p.textContent = `(${timeHour}:${timeMinute} EST >>> ${timeHour - 2}:${timeMinute} MST)`;
+    console.log(timeHour);
 })
