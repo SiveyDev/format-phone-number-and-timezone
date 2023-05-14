edb48ecb1473c565dc524a5aa00c30fb3032e9ea
SiveyDev <sivey@protonmail.com>
2023-05-13 20:32:56 -0400

    Add name into format.

--- scripts/script.js
+++ scripts/script.js
@@ -7,8 +7,10 @@ format.addEventListener("click", function(){
     textValue = inputText.value;
     let timeHour = textValue[1] + textValue[2];
     let timeMinute = textValue[4] + textValue[5];
+    let colon = textValue.lastIndexOf(':');
+    let name = textValue.slice(8, colon);
     //p.textContent = textValue;
     //console.log(inputText.value);
-    p.textContent = `(${timeHour}:${timeMinute} EST >>> ${timeHour - 2}:${timeMinute} MST)`;
+    p.textContent = `(${timeHour}:${timeMinute} EST >>> ${timeHour - 2}:${timeMinute} MST) ${name}: +1`;
     console.log(timeHour);
 })
